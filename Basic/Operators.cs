c1cd3aad210264f88c420b269170021cb3728011
Megha013 <meghaawasthi013@gmail.com>
2024-10-17 20:33:45 +0530

    Add project files.

---
+++ Basic/Operators.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class Operators
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 12;
+            int num2 = 15;
+            int num3 = 23;
+
+            //Airthmetic
+            Console.WriteLine(num1+num2);
+            Console.WriteLine(num2-num1);
+            Console.WriteLine(num1*num2);
+            Console.WriteLine(num1/num2);
+
+            // Relational - >,<,>=,<=
+            Console.WriteLine(num1 > num2);
+            Console.WriteLine(num1 < num2);
+            Console.WriteLine(num1 >= num2);
+            Console.WriteLine(num1 <= num2);
+            Console.WriteLine(num1 <= num2);
+            Console.WriteLine(num1 == num2);
+            Console.WriteLine(num1 != num2);
+
+           // Logical 
+           Console.WriteLine(num1 <= num2 && num1 <= num3);
+           Console.WriteLine(num1 >= num2 || num1 <= num3);
+        }
+    }
+}
---
+++ Basic/UserInput.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Basic
+//{
+//    internal class UserInput
+//    {
+//        static void Main(string[] args)
+//        { 
+//            Console.WriteLine("Enter your name : ");
+//            string name = Console.ReadLine();
+
+//            Console.WriteLine("Enter your age : ");
+//            int age = Convert.ToInt32(Console.ReadLine());
+
+//            Console.WriteLine("Enter your percentage : ");
+//            double per = Convert.ToDouble(Console.ReadLine());
+
+//            Console.WriteLine("${Hello} {name} and age {} and per={percentage}");
+//        }
+//    }
+//}
